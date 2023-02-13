e984cfa177c986230f58a4d8111bc6966113ca55
signlog2 <31589275+signlog2@users.noreply.github.com>
2023-02-13 15:42:16 +0100

    Update gameScene.js

--- src/js/gameScene.js
+++ src/js/gameScene.js
@@ -1,19 +1,26 @@
 import Phaser from 'phaser'
 
 
-class GameScene extends Phaser.Scene {
+export default class GameScene extends Phaser.Scene {
+   constructor() {
+      super();
+
+      this.canvasHeight;
+      this.canvasWidth;
+   }
 
    preload() {
-
+      this.load.image('bg', '/assets/background_placeholder.png');
    }
 
    create() {
+      this.canvasHeight = this.sys.game.canvas.height;
+      this.canvasWidth = this.sys.game.canvas.width;
 
+      this.add.image(this.canvasWidth / 2, this.canvasHeight / 2, 'bg');
    }
 
    update() {
 
    }
 }
-
-export default GameScene
